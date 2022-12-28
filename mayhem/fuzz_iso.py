df651d45037a0b95022e2cdaf3f429683de88fc0
bcapuano <bcapuano@asu.edu>
2022-12-28 14:43:11 -0500

    Fix

--- mayhem/fuzz_iso.py
+++ mayhem/fuzz_iso.py
@@ -14,16 +14,11 @@ with atheris.instrument_imports(include=['pycdlib']):
 from pycdlib.pycdlibexception import PyCdlibException
 iso = pycdlib.PyCdlib()
 def TestOneInput(data):
-    if len(data) < 2048:
-        return -1
-
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     try:
         with fdp.ConsumeMemoryFile(all_data=True) as fp:
             iso.open_fp(fp)
             iso.list_children(iso_path='/')
-            # for _ in iso.list_children(iso_path='/'):
-            #     pass
             iso.close()
     except (PyCdlibException, struct.error, IndexError) as e:
         return -1
